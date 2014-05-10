bf72e8f573d6ee9e3d667c57b12f0d4f15ed5016
gary <garylai1990@gmail.com>
2014-05-10 20:00:32 +0800

    Github like

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,23 +1,24 @@
 
 $(document).ready(function () {
-  var clip = new ZeroClipboard($('.copy-button'));
 
-  clip.on('load', function (clip) {
-    clip.on('mouseover', function ( event ) {
-      $(this).tooltip('hide').attr('data-original-title', 'copy to clipboard').tooltip('show');
-    });
+  var button = $('.copy-button');
+  button.tooltip();
+  var clip = new ZeroClipboard(button);
 
-    clip.on('mouseout', function ( event ) {
-      $(this).tooltip('hide');
-    });
+  clip.on('mouseover', function ( event ) {
+    $(this).attr('data-original-title', 'copy to clipboard').tooltip('show');
+  });
 
-    clip.on('complete', function ( event ) {
-      console.log('copied');
-      $(this).tooltip('hide').attr('data-original-title', 'copied').tooltip('show');
-    });
-    clip.on( 'wrongflash noflash', function() {
-      ZeroClipboard.destroy();
-    });
+  clip.on('mouseout', function ( event ) {
+    $(this).tooltip('hide');
+  });
+
+  clip.on('complete', function ( event ) {
+    console.log('copied');
+    $(this).attr('data-original-title', 'copied').tooltip('show');
+  });
+  clip.on( 'wrongflash noflash', function() {
+    ZeroClipboard.destroy();
   });
 
 });
